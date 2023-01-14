5213472e99419c511eff1d2600545b07cfa15851
HAC22 <deampuleadd@yandex.com>
2023-01-14 08:10:03 +0100

    longestSubstringOfUniqueCharacters

---
+++ SlidingWindow/LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,39 @@
+// Given a string s, find the length of the longest
+// substring
+//  without repeating characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+let longestSubstringOfUniqueCharacters = (string) => {
+  let uniqueChars = [];
+  let maxStringLength = 0;
+  let currentStringLength = 0;
+
+  for (let end = 0; end < string.length; end++) {
+    if (!uniqueChars.includes(string[end])) {
+      //console.log(`pushing ${string[end]}`);
+      uniqueChars.push(string[end]);
+    } else {
+      while (uniqueChars.includes(string[end])) {
+        //console.log(`removing ${string[end]}`);
+        uniqueChars.shift();
+      }
+
+      uniqueChars.push(string[end]);
+    }
+
+    currentStringLength = uniqueChars.length;
+
+    if (maxStringLength < currentStringLength) {
+      maxStringLength = Math.max(maxStringLength, currentStringLength);
+    }
+  }
+  //console.log(uniqueChars);
+  return maxStringLength;
+};
+
+console.log(longestSubstringOfUniqueCharacters("aab"));
