39ca98d62aa18ad25104126de5e40eb97cfdc4b4
HAC22 <deampuleadd@yandex.com>
2023-03-16 22:28:09 +0100

    min size sub array v0.1

---
+++ PHP Algorithms/Swift Algorithms/Sliding Window/MinSizeSubArray.swift
@@ -0,0 +1,35 @@
+// target = 7, nums = [2,3,1,2,4,3]
+// [1, 1, 1, 1, 1, 1, 23] O(2n)
+// currentSum = nums[start]
+// digits = 0
+// start = 0
+
+// for i = 1; i < count(nums); i++ {
+
+// 	if(currentSum >= target)
+//   {
+  
+//   	if(i-start < digits)
+//     {
+    
+//     	digits = i-start
+      
+//     }
+    
+//     currentSum -= nums[start]
+    
+//     start += 1
+    
+//     i -= 1
+    
+// 	} else {
+  
+// 	currentSum += nums[i]
+  
+//   	}
+
+// 	}
+  
+//   return digit
+  
+// }
